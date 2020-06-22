341dc8495192ab5f300670bb9e3deed781ea7003
Yash <yashshirbhate@pm.me>
2020-06-22 22:05:09 +0530

    added webview and snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,425 +1,86 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
+
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      initialRoute: MyHomePage.id,
-      routes: {
-        MyHomePage.id: (context) => MyHomePage(),
-        Registration.id: (context) => Registration(),
-        Login.id: (context) => Login(),
-        Chat.id: (context) => Chat(),
-      },
-    );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  static const String id = "HOMESCREEN";
+  Completer<WebViewController> _controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  width: 100.0,
-                  child: Image.asset("assets/images/logo.png"),
-                ),
-              ),
-              Text(
-                "Tensor Chat",
-                style: TextStyle(
-                  fontSize: 40.0,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          CustomButton(
-            text: "Log In",
-            callback: () {
-              Navigator.of(context).pushNamed(Login.id);
-            },
-          ),
-          CustomButton(
-            text: "Register",
-            callback: () {
-              Navigator.of(context).pushNamed(Registration.id);
-            },
-          )
-        ],
-      ),
-    );
-  }
-}
+          Center(
+            child: Builder(
+              builder: (context) => FlatButton.icon(onPressed: (){
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                   content: Text('Thanks for helping with the testing!'),
+                    action: SnackBarAction(
+                      label: 'Source Code',
+                      onPressed: () {
+                        WebView(
+                        initialUrl: 'github.com/yashimself',
+                          javascriptMode: JavascriptMode.unrestricted,
+                          onWebViewCreated: (WebViewController controller){
 
-class CustomButton extends StatelessWidget {
-  final VoidCallback callback;
-  final String text;
+                          _controller.complete(controller);
+                          },
 
-  const CustomButton({Key key, this.callback, this.text}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Material(
-        color: Colors.blueGrey,
-        elevation: 6.0,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: callback,
-          minWidth: 200.0,
-          height: 45.0,
-          child: Text(text),
-        ),
-      ),
-    );
-  }
-}
+                        );
 
-class Registration extends StatefulWidget {
-  static const String id = "REGISTRATION";
-  @override
-  _RegistrationState createState() => _RegistrationState();
-}
 
-class _RegistrationState extends State<Registration> {
-  String email;
-  String password;
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<void> registerUser() async {
-    FirebaseUser user = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => Chat(
-          user: user,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Tensor Chat"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: Hero(
-              tag: 'logo',
-              child: Container(
-                child: Image.asset(
-                  "assets/images/logo.png",
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 40.0,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => email = value,
-            decoration: InputDecoration(
-              hintText: "Enter Your Email...",
-              border: const OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(
-            height: 40.0,
-          ),
-          TextField(
-            autocorrect: false,
-            obscureText: true,
-            onChanged: (value) => password = value,
-            decoration: InputDecoration(
-              hintText: "Enter Your Password...",
-              border: const OutlineInputBorder(),
-            ),
-          ),
-          CustomButton(
-            text: "Register",
-            callback: () async {
-              await registerUser();
-            },
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class Login extends StatefulWidget {
-  static const String id = "LOGIN";
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  String email;
-  String password;
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<void> loginUser() async {
-    FirebaseUser user = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => Chat(
-          user: user,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Tensor Chat"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: Hero(
-              tag: 'logo',
-              child: Container(
-                child: Image.asset(
-                  "assets/images/logo.png",
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 40.0,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => email = value,
-            decoration: InputDecoration(
-              hintText: "Enter Your Email...",
-              border: const OutlineInputBorder(),
-            ),
-          ),
-          SizedBox(
-            height: 40.0,
-          ),
-          TextField(
-            autocorrect: false,
-            obscureText: true,
-            onChanged: (value) => password = value,
-            decoration: InputDecoration(
-              hintText: "Enter Your Password...",
-              border: const OutlineInputBorder(),
-            ),
-          ),
-          CustomButton(
-            text: "Log In",
-            callback: () async {
-              await loginUser();
-            },
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class Chat extends StatefulWidget {
-  static const String id = "CHAT";
-  final FirebaseUser user;
-
-  const Chat({Key key, this.user}) : super(key: key);
-  @override
-  _ChatState createState() => _ChatState();
-}
-
-class _ChatState extends State<Chat> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final Firestore _firestore = Firestore.instance;
-
-  TextEditingController messageController = TextEditingController();
-  ScrollController scrollController = ScrollController();
-
-  Future<void> callback() async {
-    if (messageController.text.length > 0) {
-      await _firestore.collection('messages').add({
-        'text': messageController.text,
-        'from': widget.user.email,
-        'date': DateTime.now().toIso8601String().toString(),
-      });
-      messageController.clear();
-      scrollController.animateTo(
-        scrollController.position.maxScrollExtent,
-        curve: Curves.easeOut,
-        duration: const Duration(milliseconds: 300),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Hero(
-          tag: 'logo',
-          child: Container(
-            height: 40.0,
-            child: Image.asset("assets/images/logo.png"),
-          ),
-        ),
-        title: Text("Tensor Chat"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              _auth.signOut();
-              Navigator.of(context).popUntil((route) => route.isFirst);
-            },
-          )
-        ],
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: _firestore
-                    .collection('messages')
-                    .orderBy('date')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData)
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-
-                  List<DocumentSnapshot> docs = snapshot.data.documents;
-
-                  List<Widget> messages = docs
-                      .map((doc) => Message(
-                    from: doc.data['from'],
-                    text: doc.data['text'],
-                    me: widget.user.email == doc.data['from'],
-                  ))
-                      .toList();
-
-                  return ListView(
-                    controller: scrollController,
-                    children: <Widget>[
-                      ...messages,
-                    ],
-                  );
-                },
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      onSubmitted: (value) => callback(),
-                      decoration: InputDecoration(
-                        hintText: "Enter a Message...",
-                        border: const OutlineInputBorder(),
-                      ),
-                      controller: messageController,
+                    },
                     ),
+                    duration: Duration(seconds: 3),
+                  ));
+              },
+                  icon: Icon(AntDesign.github),
+                label: Text(''),
                   ),
-                  SendButton(
-                    text: "Send",
-                    callback: callback,
-                  )
-                ],
-              ),
+            )
             ),
-          ],
-        ),
+          ]
       ),
-    );
-  }
-}
 
-class SendButton extends StatelessWidget {
-  final String text;
-  final VoidCallback callback;
+      appBar: AppBar(
+        title: Text('X',
+        textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          InkWell(
+            child: RaisedButton(onPressed: (){
+              WebView(
+                initialUrl: 'https://github.com/yashimself/x',
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController controller){
 
-  const SendButton({Key key, this.text, this.callback}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return FlatButton(
-      color: Colors.orange,
-      onPressed: callback,
-      child: Text(text),
-    );
-  }
-}
+                  _controller.complete(controller);
+                },
 
-class Message extends StatelessWidget {
-  final String from;
-  final String text;
+              );
 
-  final bool me;
-
-  const Message({Key key, this.from, this.text, this.me}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment:
-        me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            from,
-          ),
-          Material(
-            color: me ? Colors.teal : Colors.red,
-            borderRadius: BorderRadius.circular(10.0),
-            elevation: 6.0,
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-              child: Text(
-                text,
-              ),
-            ),
+            },
+              color: Colors.white70,
+              child: Text('Click me'),
+              elevation: 2.0,
+    ),
           )
         ],
       ),
